0f1e1aa38db6fbcffd6fb6695d87f5f0d5b356b4
psam20 <unique.kishan.gupta@gmail.com>
2020-10-06 08:56:15 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="app">
+    <h1>
+      Demonstration of passing Data Between Components,styles Demo
+    </h1>
+    <h4 v-if="recent">
+      Recently Added Book :- {{Recently.title.toUpperCase()}},{{Recently.publication}},${{Recently.price}}
+      
+    </h4>
+    <hr/>
+    <Book
+   
+     :books="Books"
+     @add-book="addBooks"
+     />
+  </div>
+</template>
+<script>
+import Book from './components/BookComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Book
+  },
+  data(){
+    return {
+      Books :[
+        {
+          title:'MongoDb in Action',
+          publication:'2011',
+          price:'38'
+        },
+         {
+          title:'Javascript: The Good Parts',
+          publication:'2008',
+          price:'21'
+        }
+      ],
+      Recently:{},
+      recent:false
+    }
+  },
+  methods:{
+    addBooks(data){
+
+      console.log(data)
+      this.Recently=data
+      this.recent=true
+      this.Books.push(data)
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1{
+  color:brown;
+}
+h4{
+   color:rgba(233,110,130,0.8)
+}
+</style>
---
+++ src/components/BookComponent.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <h1 class="bookHeader">Book List Technology</h1>
+  </div>
+   <div class="bookList"> 
+       <table align="center">
+         <tr>
+           <th>
+             Title
+           </th>
+           <th>Publication Year</th>
+           <th>Price</th>
+         </tr>
+         <tr v-for="book in books">
+           <td>{{book.title.toUpperCase()}} </td>
+           <td>{{book.publication}}</td>
+           <td>${{book.price}}</td>
+         </tr>
+       </table>
+   </div>
+   <div class="addBookList">
+     Enter Book Title:- <input type="text" v-model="bookTitle" />
+     Enter Publication Year <input type="text" v-model="publicationYear" />
+     Enter Price <input type="string" v-model="price"  />
+   </div>
+   <div class="addBook">
+     <button @click="addBook">
+       Add Book
+     </button>
+   </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'Book',
+  props: [
+    'books'
+  ],
+  emits:['add-book'],
+  data(){
+    return{
+      bookTitle :'',
+      publicationYear:'',
+      price:''
+
+    }
+  },
+  methods:{
+    addBook(){
+      if(this.bookTitle == '' || this.publicationYear=='' || this.price ==''){
+        console.log("obj is empty")
+        return
+      }
+      // console.log("this is bookTitle",this.bookTitle)
+      this.$emit('add-book',{
+        title:this.bookTitle,
+        publication:this.publicationYear,
+        price:this.price
+      })
+      this.bookTitle='';
+      this.publicationYear='';
+      this.price='';
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.bookHeader{
+  color:blue;
+}
+table,th,td {
+  border : 1px solid black;
+  color:rgba(233,132,34,0.8)
+}
+td{
+  color:rgba(233,110,120,0.6)
+}
+.bookList, .addBookList {
+  margin-bottom :20px;
+}
+</style>
